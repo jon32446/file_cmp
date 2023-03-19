3dd2b0f55a2e38fccd815d305918c7c6e2c30c35
Jon <dowlingjh@gmail.com>
2023-03-19 21:48:20 +0200

    Added clap CLI parser

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,19 @@
+use clap::Parser;
 use file_cmp::compare_files;
 
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// File path
+    file1: String,
+    /// File path
+    file2: String,
+}
+
 fn main() {
-    match compare_files("test.txt", "testing.txt") {
+    let args = Args::parse();
+
+    match compare_files(&args.file1, &args.file2) {
         Ok(None) => println!("Files are equal"),
         Ok(Some(pos)) => println!("Files differ at offset {}", pos),
         Err(e) => eprintln!("Error: {}", e),
