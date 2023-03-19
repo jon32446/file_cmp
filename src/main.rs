89140eee909647f69e1f427b6660ed296ca10775
Jon <dowlingjh@gmail.com>
2023-03-19 22:57:21 +0200

    Updated to use ExitCode

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,32 @@
+use std::process::ExitCode;
+
 use clap::Parser;
 use file_cmp::compare_files;
 
 #[derive(Parser, Debug)]
-#[command(author, version, about, long_about = None)]
+#[command(author, version, about, long_about = None)] // Read from `Cargo.toml`
 struct Args {
-    /// File path
+    /// File path to first file to compare
     file1: String,
-    /// File path
+    /// File path to second file to compare
     file2: String,
 }
 
-fn main() {
+fn main() -> ExitCode {
     let args = Args::parse();
 
     match compare_files(&args.file1, &args.file2) {
-        Ok(None) => println!("Files are equal"),
-        Ok(Some(pos)) => println!("Files differ at offset {}", pos),
-        Err(e) => eprintln!("Error: {}", e),
+        Ok(None) => {
+            println!("Files are equal");
+            ExitCode::SUCCESS
+        }
+        Ok(Some(offset)) => {
+            eprintln!("Files differ at byte {}", offset);
+            ExitCode::SUCCESS
+        }
+        Err(e) => {
+            eprintln!("Error: {}", e);
+            ExitCode::FAILURE
+        }
     }
 }
