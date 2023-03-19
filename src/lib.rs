fc364e9fc4538da324fa312b3031beb98ba22c7e
Jon <dowlingjh@gmail.com>
2023-03-19 21:15:57 +0200

    Re-organised and changed to integration test

---
+++ src/lib.rs
@@ -0,0 +1,38 @@
+use std::fs::{self, File};
+use std::io::{self, BufReader, Read};
+use std::path::Path;
+
+pub fn compare_files<P: AsRef<Path>>(file1_path: P, file2_path: P) -> io::Result<Option<usize>> {
+    let file1_meta = fs::metadata(&file1_path)?;
+    let file2_meta = fs::metadata(&file2_path)?;
+
+    if file1_meta.len() == 0 || file2_meta.len() == 0 {
+        return Ok(Some(0));
+    }
+
+    let mut file1 = BufReader::new(File::open(file1_path)?);
+    let mut file2 = BufReader::new(File::open(file2_path)?);
+
+    let mut buffer1 = [0; 4096];
+    let mut buffer2 = [0; 4096];
+    let mut pos = 0;
+
+    loop {
+        let len1 = file1.read(&mut buffer1)?;
+        let len2 = file2.read(&mut buffer2)?;
+
+        if len1 == 0 && len2 == 0 {
+            return Ok(None);
+        }
+
+        if len1 != len2 || buffer1[..len1] != buffer2[..len2] {
+            for i in 0..len1 {
+                if buffer1[i] != buffer2[i] {
+                    return Ok(Some(pos + i));
+                }
+            }
+        }
+
+        pos += len1;
+    }
+}
